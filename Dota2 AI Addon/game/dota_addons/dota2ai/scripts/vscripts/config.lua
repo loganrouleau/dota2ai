f38d928c9ea421912a5195c036e7543fb0262cb8
Logan Rouleau <loganrouleau@users.noreply.github.com>
2018-01-07 13:40:25 -0800

    Deleted unused files

--- Dota2 AI Addon/game/dota_addons/dota2ai/scripts/vscripts/config.lua
+++
@@ -1,9 +0,0 @@
--- Set the enemy mid hero. The game may not have behaviours for every hero. The player bot will be picked using a JSON query
-Dota2AI.useEnemyHero = true
-Dota2AI.enemyHero = "npc_dota_hero_lycan"
-Dota2AI.enemyHeroDifficulty = "hard" -- {"passive", "easy", "normal", "hard", "unfair"}
-
-Dota2AI.baseURL = "http://localhost:8080/Dota2AIService/api/service"
-
--- Set this to 0 to skip the configuration dialog
-Dota2AI.ConfigUITimeout = 0
